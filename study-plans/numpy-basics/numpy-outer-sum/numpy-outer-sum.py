e19e94156c7075baae0feeeec9ce495f2cc72845
Nasrul Huda <nasrul@syntwin.ai>
2026-04-24 17:42:37 +0200

    Update solution for numpy-outer-sum

--- study-plans/numpy-basics/numpy-outer-sum/numpy-outer-sum.py
+++ study-plans/numpy-basics/numpy-outer-sum/numpy-outer-sum.py
@@ -2,11 +2,24 @@ import numpy as np
 
 def outer_sum(a, b):
     """Returns: np.ndarray of shape (m, n), outer sum where out[i,j] = a[i] + b[j]"""
-    res = []
-    for i in range(len(a)):
-        row_to_app = []
-        for j in range(len(b)):
-            row_to_app.append(a[i] + b[j])
-        res.append(row_to_app)
 
-    return np.array(res, dtype=np.float64)
+    # Using the standard add.outer
+
+    return np.add.outer(np.array(a, dtype=np.float64), np.array(b, dtype=np.float64))
+
+    # a = np.array(a, dtype=np.float64)
+    # b = np.array(b, dtype=np.float64)
+
+    # out = a[:, np.newaxis] + b[np.newaxis, :]
+    # return out
+
+    # Another Solution
+    
+    # res = []
+    # for i in range(len(a)):
+    #     row_to_app = []
+    #     for j in range(len(b)):
+    #         row_to_app.append(a[i] + b[j])
+    #     res.append(row_to_app)
+
+    # return np.array(res, dtype=np.float64)
